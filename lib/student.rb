1e6cfe40c58ee3800cfa499aeed8de8649ad3251
github username <github email address>
2019-06-13 19:32:47 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ require_relative "../config/environment.rb"
     def initialize(id=nil, name, grade)
       @id = id
       @name = name
-      @grade =grade
+      @grade = grade
     end
 
     def self.create_table
