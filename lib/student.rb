1b93d2237d88f90cd0deed96ad83ce3b03206b44
github username <github email address>
2019-06-13 12:25:08 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -55,13 +55,6 @@ class Student
     student
   end
 
-  #def self.new_from_db(row)
-   # id = row[0]
-   # name = row[1]
-   # grade = row[2]
-   # Student.new(id, name, grade)
- # end
-
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
